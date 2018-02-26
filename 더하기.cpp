c9c7b70e5a9551e58403590e95c7818e4f3c3cd0
SongTongil <tong9433@naver.com>
2018-02-26 16:51:42 +0900

    첫알고리즘

---
+++ 더하기.cpp
@@ -0,0 +1,15 @@
+include <iostream>
+
+using namespace std; // std:: 를 생략 가능
+
+int main() {
+    int a;
+    int b;
+
+    cin >> a >> b; // a와 b를 하나씩 입력 받기
+    cout << a+b << '\n';
+
+    return 0;
+}
+
+
